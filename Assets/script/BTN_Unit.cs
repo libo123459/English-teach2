060e30f4d16a31bdadd0e865f5f490e26645021d
LiBo <libo123459@126.com>
2018-11-12 16:16:12 +0800

    Initial commit

---
+++ Assets/script/BTN_Unit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BTN_Unit : MonoBehaviour {
+    public int UnitNum;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/script/Manage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manage : MonoBehaviour {
+    public Transform panel_unit;
+    public BTN_Unit btn;
+	// Use this for initialization
+	void Start () {
+        Unit_btn_init();
+	}
+    public void Unit_btn_init()
+    {
+        for (int i = 1; i <= 20; i++)
+        {
+            BTN_Unit temp = Instantiate(btn,panel_unit);
+            temp.UnitNum = i;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
